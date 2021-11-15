be813abca29f79f2257186375e30133c8bec1b08
Omeega1 <valeentin91@live.fr>
2021-11-15 09:26:42 -0500

    Fonctionne

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloadField, FieldList, IntegerField, SelectMultipleField
-from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms import StringField, SubmitField, FloatField, IntegerField
+from wtforms.validators import ValidationError, DataRequired
 
 class AddIngredientForm(FlaskForm):
-    name = StringField('Nom', validators=[DataRequired()])
+    name = StringField('Nom', validators = [DataRequired()])
     cost = FloatField('Coût',default=0.0)
     submit = SubmitField('Ajouter')
 
 class AddRecetteForm(FlaskForm):
     name = StringField('Nom', validators = [DataRequired()])
-    ingredients = SelectMultipleField("Ingrédients", choices=[("Carottes"),("Poivrons"),("Tomate")])
+    ingredients = StringField("Ingrédients")
     submit = SubmitField("Terminer la recette")
