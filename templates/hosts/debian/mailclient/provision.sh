25104650f411b6ad18e5e46e6f96236fabe1e7fb
karkat413 <jeannexlbskf@gmail.com>
2021-11-20 16:25:59 +0100

    Fixed an issue when creating a mail client

--- templates/hosts/debian/mailclient/provision.sh
+++ templates/hosts/debian/mailclient/provision.sh
@@ -14,6 +14,11 @@ if [ -z $login ] ; then
   login="debian"
 fi
 
+if [ ! -d "/home/$login" ];then
+mkdir /home/$login
+fi
+
+
 cp -ar claws-mail /home/$login/.claws-mail
 #chown -R $login /home/$login/.claws-mail
 chmod -R 777 /home/$login/.claws-mail
